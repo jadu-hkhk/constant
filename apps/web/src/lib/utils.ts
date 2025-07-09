644aa1c6df0eb4c2d351ed84b0c31b618dbc54d0
Himanshu <158589336+jadu-hkhk@users.noreply.github.com>
2025-07-10 00:36:26 +0530

    chore(api): add dockerfile and fix build issues

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -5,4 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_API_BACKEND_URL ?? "http://localhost:5000" + "/api/v1"
+export const ENV_BACKEND_URL = process.env.NEXT_PUBLIC_API_BACKEND_URL ?? "http://localhost:5000"
+export const BACKEND_URL = `${ENV_BACKEND_URL}/api/v1`
